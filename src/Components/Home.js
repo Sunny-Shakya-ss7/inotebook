5306dc5195f4d447dbc50f88763a7628e2b48471
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-09-09 13:19:24 +0545

    Adding NoteItem in a Separate Note component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,6 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/NoteContext";
+import { Notes } from "./Notes";
 
 export const Home = () => {
-  const context = useContext(noteContext);
-  const { notes, setNotes } = context;
-
   return (
     <div>
       <div className="container my-3">
@@ -50,12 +46,7 @@ export const Home = () => {
         </form>
       </div>
 
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note) => {
-          return note.title;
-        })}
-      </div>
+      <Notes />
     </div>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,62 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const notesInitial = {};
+  const notesInitial = [
+    {
+      user: "613735b8d81901092ebf3eb4",
+      title: "My Title",
+      description: "Wake up early",
+      tag: "personal",
+      _id: "6139b73218973a5272f23150",
+      date: "2021-09-09T07:26:42.258Z",
+      __v: 0,
+    },
+    {
+      user: "613735b8d81901092ebf3eb4",
+      title: "My Title",
+      description: "Wake up early",
+      tag: "personal",
+      _id: "6139b73218973a5272f23150",
+      date: "2021-09-09T07:26:42.258Z",
+      __v: 0,
+    },
+    {
+      user: "613735b8d81901092ebf3eb4",
+      title: "My Title",
+      description: "Wake up early",
+      tag: "personal",
+      _id: "6139b73218973a5272f23150",
+      date: "2021-09-09T07:26:42.258Z",
+      __v: 0,
+    },
+    {
+      user: "613735b8d81901092ebf3eb4",
+      title: "My Title",
+      description: "Wake up early",
+      tag: "personal",
+      _id: "6139b73218973a5272f23150",
+      date: "2021-09-09T07:26:42.258Z",
+      __v: 0,
+    },
+    {
+      user: "613735b8d81901092ebf3eb4",
+      title: "My Title",
+      description: "Wake up early",
+      tag: "personal",
+      _id: "6139b73218973a5272f23150",
+      date: "2021-09-09T07:26:42.258Z",
+      __v: 0,
+    },
+    {
+      user: "613735b8d81901092ebf3eb4",
+      title: "My Title",
+      description: "Wake up early",
+      tag: "personal",
+      _id: "6139b73218973a5272f23150",
+      date: "2021-09-09T07:26:42.258Z",
+      __v: 0,
+    },
+  ];
   const [notes, setNotes] = useState(notesInitial);
 
   return (
