590256ad47386a94eefa15e9dec76e11c28b8a1e
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-09-09 14:17:04 +0545

    Adding font awesome icons

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Home";
 import { About } from "./Components/About";
 import NoteState from "./context/notes/NoteState";
+import { Alert } from "./Components/Alert";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="Amazing!!!" />
           <div className="container">
             <Switch>
               <Route exact path="/">
